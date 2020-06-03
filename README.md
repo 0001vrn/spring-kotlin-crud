be2fddde86a4db7163e777f9b9583695713f9ece
0001vrn <vthakur@expediagroup.com>
2020-06-03 22:59:18 +0530

    update docs, dao, libs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-kotlin-crud
+
+To run this project, follow these steps:
+
+* Run the application database by executing `docker-compose up` in src/test/resources directory.
+* Launch the Spring Boot Application (ApplicationRunner). 
+* By default, the application will connect to the database(MySQL) (configuration in *application.properties*)
--- src/main/kotlin/varun/bootdemo/TodoResource.kt
+++ src/main/kotlin/varun/bootdemo/TodoResource.kt
@@ -4,36 +4,45 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import java.lang.Exception
+import java.sql.Timestamp
 import java.time.Instant
+import java.util.UUID
 import javax.persistence.*
 
 interface TodoRepository : JpaRepository<Todo, Long>
 
-@RestController @RequestMapping(value = "/todo") @EnableWebMvc
+@RestController @RequestMapping(value = ["/todo"]) @EnableWebMvc
 class TodoResource(val todoRepository: TodoRepository) {
 
-    @GetMapping(value = "/")
+    @GetMapping(value = ["/"])
     fun getAll() = todoRepository.findAll()
 
-    @GetMapping(value = "/{id}")
+    @GetMapping(value = ["/{id}"])
     fun getOne(@PathVariable id: Long) = todoRepository.findById(id)
 
-    @PostMapping(value = "/")
+    @PostMapping(value = ["/"])
     fun new(@RequestBody text: String) = todoRepository.save(Todo(text = text))
 
-    @DeleteMapping(value = "/{id}")
+    @DeleteMapping(value = ["/{id}"])
     fun delete(@PathVariable id: Long) = todoRepository.deleteById(id)
 
-    @PutMapping(value = "/{id}")
+    @PutMapping(value = ["/{id}"])
     fun update(@PathVariable id: Long, @RequestBody todo: Todo): Todo {
         val toUpdate: Todo = todoRepository.findById(id).orElseThrow{ Exception("server error") }
         toUpdate.text = todo.text
         toUpdate.done = todo.done
+        toUpdate.updatedAt = Timestamp.from(Instant.now())
         return todoRepository.save(toUpdate)
     }
 }
 
 
 @Entity
-class Todo(@Id @GeneratedValue(strategy = GenerationType.AUTO)
-        val Id: Long = 0, var text: String = "", var done: Boolean = false, var createdAt: Instant = Instant.now())
+class Todo(
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    val uuid: UUID = UUID.randomUUID(),
+    var text: String = "",
+    var done: Boolean = false,
+    var createdAt: Timestamp = Timestamp.from(Instant.now()),
+    var updatedAt: Timestamp = Timestamp.from(Instant.now())
+)
